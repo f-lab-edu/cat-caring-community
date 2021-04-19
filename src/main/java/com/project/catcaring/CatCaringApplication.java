f5b397d2f848b6309b5b8f86a87d8d1b170e07c7
sunny <parks38@uw.edu>
2021-04-19 10:18:38 +0900

    feat : 환경 초기 세팅

---
+++ src/main/java/com/project/catcaring/CatCaringApplication.java
@@ -0,0 +1,13 @@
+package com.project.catcaring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatCaringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CatCaringApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/project/catcaring/CatCaringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.catcaring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatCaringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
