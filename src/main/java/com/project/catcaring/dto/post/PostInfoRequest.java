147b733d97feec62aa7bba3b559c6aac7f95a278
sunny <parks38@uw.edu>
2021-05-18 15:38:46 +0900

    feat: post save request dto

---
+++ src/main/java/com/project/catcaring/dto/post/PostInfoRequest.java
@@ -0,0 +1,23 @@
+package com.project.catcaring.dto.post;
+
+import com.project.catcaring.domain.user.Authority;
+import com.project.catcaring.domain.user.User.Address;
+import java.util.List;
+import lombok.Getter;
+import reactor.util.annotation.Nullable;
+
+@Getter
+public class PostInfoRequest {
+  private String title;
+  private String content;
+  @Nullable
+  private Authority postAuthorityCode;
+  @Nullable
+  private List<String> tagNames;
+  private Address location; //enum
+  @Nullable
+  private String locationDetail;
+  @Nullable
+  private Authority locationAuthorityCode;
+
+}
