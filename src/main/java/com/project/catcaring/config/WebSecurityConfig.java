b6574add93a7ff556d29d5ac2f5be38119066cbd
sunny <parks38@uw.edu>
2021-05-18 21:46:46 +0900

    fix: security config 수정

--- src/main/java/com/project/catcaring/config/WebSecurityConfig.java
+++ src/main/java/com/project/catcaring/config/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable().authorizeRequests()
-        .antMatchers("/auth/**", "/image/**", "/", "/resources/**" ,"/user/**", "/mappers/**").permitAll()
+        .antMatchers("/auth/**", "/posts/**", "/", "/resources/**" ,"/user/**", "/mappers/**").permitAll()
         .anyRequest().authenticated();
   }
    //로그인 인증 절차 확인
