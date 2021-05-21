220123f536cbc40c44c730c64d1da027604fba85
sunny <parks38@uw.edu>
2021-05-21 13:44:48 +0900

    fix: 게시물 수정 controller 기능 추가

--- src/main/java/com/project/catcaring/controller/PostController.java
+++ src/main/java/com/project/catcaring/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.project.catcaring.controller;
 
+import com.project.catcaring.domain.Post;
 import com.project.catcaring.dto.post.PostInfoRequest;
+import com.project.catcaring.dto.post.PostUpdateRequest;
 import com.project.catcaring.service.LoginSessionService;
 import com.project.catcaring.service.PostServiceImpl;
 import com.project.catcaring.service.UserService;
@@ -10,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,13 +44,29 @@ public class PostController {
     log.info(loginSessionService.getCurrentUsername(session) + "님이 게시물 번호 :" + postId + " 포스트를 삭제를 요청했습니다.");
     try{
       postService.deletePost(userId, postId);
-      log.info("게시물 번호: " + postId + " 포스트가 삭제되었습니다. ");
+      log.info("게시물 번호: " + postId + " 포스트가 삭제 되었습니다. ");
       return HttpStatus.OK;
     } catch (RuntimeException e) {
       log.warn("게시물 번호: " + postId + " 포스트 삭제 중 오류가 발생했습니다. ");
       e.printStackTrace();
       return HttpStatus.NON_AUTHORITATIVE_INFORMATION;
     }
+  }
+
+  @PatchMapping("/update")
+  public HttpStatus updatePost(@RequestBody PostUpdateRequest postUpdateRequest, HttpSession session) {
+    Long userId = userService.getUserId(loginSessionService.getCurrentUsername(session));
+    log.info(loginSessionService.getCurrentUsername(session) + "님이 게시물 번호 :" + postUpdateRequest.getPostId() + " 포스트 수정을 요청했습니다.");
+
+    try {
+      postService.updatePost(postUpdateRequest, userId);
+      log.info("게시물 번호: " + postUpdateRequest.getPostId() + " 포스트가 수정 되었습니다.");
+    } catch (RuntimeException e) {
+      e.printStackTrace();
+      return HttpStatus.NON_AUTHORITATIVE_INFORMATION;
+    }
+
+    return HttpStatus.OK;
   }
 
 
