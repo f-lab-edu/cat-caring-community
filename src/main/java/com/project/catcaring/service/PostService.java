a7eafe1ed9f1b9e44710bbb6c1653f1d5833db35
sunny <parks38@uw.edu>
2021-05-21 13:45:45 +0900

    fix: 게시물 수정 service parameter 수정

--- src/main/java/com/project/catcaring/service/PostService.java
+++ src/main/java/com/project/catcaring/service/PostService.java
@@ -2,13 +2,14 @@ package com.project.catcaring.service;
 
 import com.project.catcaring.domain.Post;
 import com.project.catcaring.dto.post.PostInfoRequest;
+import com.project.catcaring.dto.post.PostUpdateRequest;
 import java.util.List;
 
 public interface PostService {
 
   void uploadPost(PostInfoRequest postInfoRequest, Long userId);
   List<Post> viewPost(Long postId);
-  void updatePost(PostInfoRequest postInfoRequest);
+  void updatePost(PostUpdateRequest postUpdateRequest, Long userId);
   void deletePost(Long userId, Long postId);
 
 
