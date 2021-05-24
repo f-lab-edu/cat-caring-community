447a52c8dca6fb84ad76fe247ac49cdb1c609127
sunny <parks38@uw.edu>
2021-05-24 23:29:50 +0900

    feat : 페이지별 게시물 목록 구현 controller

--- src/main/java/com/project/catcaring/controller/PostController.java
+++ src/main/java/com/project/catcaring/controller/PostController.java
@@ -1,22 +1,32 @@
 package com.project.catcaring.controller;
 
 import com.project.catcaring.domain.Post;
+import com.project.catcaring.dto.post.PageRequest;
 import com.project.catcaring.dto.post.PostInfoRequest;
+import com.project.catcaring.dto.post.PostListInfo;
 import com.project.catcaring.dto.post.PostUpdateRequest;
 import com.project.catcaring.service.LoginSessionService;
 import com.project.catcaring.service.PostServiceImpl;
 import com.project.catcaring.service.UserService;
+import java.util.List;
 import javax.servlet.http.HttpSession;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -67,6 +77,12 @@ public class PostController {
     }
 
     return HttpStatus.OK;
+  }
+
+  @GetMapping("/{pageId}")
+  public List<PostListInfo> getPostLists(@PathVariable("pageId") int page) {
+    PageRequest pageRequest = PageRequest.builder().page(page).build();
+    return postService.findPostLists(pageRequest);
   }
 
 
