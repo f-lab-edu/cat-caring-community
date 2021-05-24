53f71385dab1188ec7b411d5977dfc855b6192d8
sunny <parks38@uw.edu>
2021-05-24 23:29:58 +0900

    feat : 페이지별 게시물 목록 구 mapper

--- src/main/java/com/project/catcaring/mapper/PostMapper.java
+++ src/main/java/com/project/catcaring/mapper/PostMapper.java
@@ -3,9 +3,14 @@ package com.project.catcaring.mapper;
 import com.project.catcaring.domain.Location;
 import com.project.catcaring.domain.Post;
 import com.project.catcaring.domain.Tag;
+import com.project.catcaring.dto.post.PageRequest;
 import com.project.catcaring.dto.post.PostContentUpdate;
+import com.project.catcaring.dto.post.PostListInfo;
 import com.project.catcaring.dto.post.PostLocationUpdate;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @Mapper
 public interface PostMapper {
@@ -17,4 +22,7 @@ public interface PostMapper {
   boolean deletePost(Long userId, Long postId);
   boolean updateContent(PostContentUpdate postContentUpdate);
   boolean updateLocation(PostLocationUpdate postLocationUpdate);
+
+  List<PostListInfo> findAllPosts(PageRequest page);
+
 }
