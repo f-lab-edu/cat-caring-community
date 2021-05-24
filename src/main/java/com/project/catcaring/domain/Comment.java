35055f5369c14e66d3e51a4ec7197e9d40b2ad87
sunny <parks38@uw.edu>
2021-05-25 02:16:01 +0900

    fix : comment 도메인 수정

--- src/main/java/com/project/catcaring/domain/Comment.java
+++ src/main/java/com/project/catcaring/domain/Comment.java
@@ -2,14 +2,17 @@ package com.project.catcaring.domain;
 
 import com.project.catcaring.domain.Post.PostStatus;
 import java.time.LocalDateTime;
+import lombok.Builder;
+import reactor.util.annotation.Nullable;
 
+@Builder
 public class Comment {
-  private Long id;
-  private Long parent_id;
-  private Long post_id;
-  private Long user_id;
-  private String comment;
-  private LocalDateTime createdAt;
-  private PostStatus status;
-
+  private final Long id;
+  @Nullable
+  private final Long commentParent;
+  private final Long postId;
+  private final Long userId;
+  private final String comment;
+  private final LocalDateTime createdAt;
+  private final PostStatus status;
 }
