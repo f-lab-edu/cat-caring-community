8b3f28f2846184349b419fabb6e5a5a9ffef58d7
sunny <parks38@uw.edu>
2021-05-27 02:51:09 +0900

    fix: usermapper 추가

--- src/main/java/com/project/catcaring/mapper/UserMapper.java
+++ src/main/java/com/project/catcaring/mapper/UserMapper.java
@@ -2,11 +2,13 @@ package com.project.catcaring.mapper;
 
 import com.project.catcaring.domain.user.User;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserMapper {
   void insertUser(User user);
   boolean isUniqueId(String username);
-  User findByUsername(@Param("username")String username);
+  User findByUsername(String username);
+  User findByUserId(Long userId);
+  void deleteUser(Long userId);
+  void update(User user);
 }
