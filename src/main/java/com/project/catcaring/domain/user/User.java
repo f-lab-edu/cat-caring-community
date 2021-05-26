992818d3a5b44b6a1921b153bd1b27182042b8bf
sunny <parks38@uw.edu>
2021-05-27 02:48:21 +0900

    fix: 도메인 내에서 builder 설정

--- src/main/java/com/project/catcaring/domain/user/User.java
+++ src/main/java/com/project/catcaring/domain/user/User.java
@@ -1,13 +1,23 @@
 package com.project.catcaring.domain.user;
 
+import com.project.catcaring.dto.user.UserChangeRequest;
+import com.project.catcaring.dto.user.UserInfoRequest;
+import com.project.catcaring.service.user.LoginSessionService;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Getter
 @Builder
+@RequiredArgsConstructor
 public class User {
+  private static final PasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
+  private static LoginSessionService loginSessionService;
+
   private final Long id;
   private final String username;
   private final String password;
@@ -31,5 +41,24 @@ public class User {
     JUNGNANG, SEONGBUK, GANGBUK, DOBONG, NOWON, EUNPYEONG, SEODAEMUN, MAPO,
     YANGCHEON, GANGSEO, GURO, GEUMCHEON, YEONGDEUNGPO, DONGJAK,
     GWANAK, SEOCHO, GANGNAM, SONGPA, GANGDONG
+  }
+
+  public static User generate(UserInfoRequest userInfoRequest) {
+    return User.builder().username(userInfoRequest.getUsername())
+        .password(PASSWORD_ENCODER.encode(userInfoRequest.getPassword()))
+        .fullName(userInfoRequest.getFullName())
+        .email(userInfoRequest.getEmail())
+        .location(userInfoRequest.getLocation())
+        .authorityCode(Authority.USER)
+        .status(Status.MEMBER)
+        .build();
+  }
+
+  public static User modify(UserChangeRequest userChangeRequest, Long userId) {
+    return User.builder()
+        .id(userId)
+        .password(PASSWORD_ENCODER.encode(userChangeRequest.getPassword()))
+        .fullName(userChangeRequest.getFullName())
+        .location(userChangeRequest.getLocation()).build();
   }
 }
