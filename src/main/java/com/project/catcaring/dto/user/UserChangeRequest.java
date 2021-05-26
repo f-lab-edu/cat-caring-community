19e8910f34b78273ca4e7ffb5a85490b633e6e47
sunny <parks38@uw.edu>
2021-05-27 02:49:21 +0900

    fix: @nonNull 로 모두 변경

--- src/main/java/com/project/catcaring/dto/user/UserChangeRequest.java
+++ src/main/java/com/project/catcaring/dto/user/UserChangeRequest.java
@@ -2,13 +2,18 @@ package com.project.catcaring.dto.user;
 
 import com.project.catcaring.domain.user.User.Address;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import reactor.util.annotation.Nullable;
 
 @Getter
 @RequiredArgsConstructor
 public class UserChangeRequest {
+  @NonNull
   private final String password;
+  @NonNull
   private final String fullName;
+  @NonNull
   private final Address location;
 
 }
