a91568247fc9e7cf95ab76705167f620e928e27d
sunny <parks38@uw.edu>
2021-05-31 22:44:07 +0900

    fix: spacing style 수정

--- src/main/java/com/project/catcaring/domain/Location.java
+++ src/main/java/com/project/catcaring/domain/Location.java
@@ -3,15 +3,12 @@ package com.project.catcaring.domain;
 import com.project.catcaring.domain.user.Authority;
 import com.project.catcaring.domain.user.User.Address;
 import lombok.Builder;
-import lombok.Getter;
 
 @Builder
 public class Location {
-
   private final Long id;
   private final Long postId;
   private final Address location;
   private final String locationDetail;
   private final Authority authorityCode;
-
 }
