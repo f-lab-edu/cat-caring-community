ed246d8884ab7fa6223384e6f435533b5d6c789c
sunny <parks38@uw.edu>
2021-05-31 22:44:42 +0900

    fix: post generator 역할 분리

--- src/main/java/com/project/catcaring/domain/Post.java
+++ src/main/java/com/project/catcaring/domain/Post.java
@@ -2,37 +2,39 @@ package com.project.catcaring.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.project.catcaring.domain.user.Authority;
+import com.project.catcaring.dto.post.PostInfoRequest;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
-import lombok.Getter;
 
 @Builder
 public class Post {
   private final Long id;
-
   private final Long userId;
-
   private final String title;
-
   private final String content;
-
   private final Authority authorityCode;
-
   @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private final LocalDateTime createdAt;
-
   @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private final LocalDateTime modifiedAt;
-
   private final PostStatus status;
-
   private final List<Tag> tagList;
-
   private final Location location;
 
   public enum PostStatus {
     CREATED, DELETED, MODIFIED
+  }
+
+  public static Post generate(PostInfoRequest postInfoRequest, Long userId) {
+    Authority authority = postInfoRequest.getPostAuthorityCode();
+    if(authority == null) {
+      authority = Authority.ALL;
+    }
+
+    return Post.builder().userId(userId).title(postInfoRequest.getTitle())
+        .content(postInfoRequest.getContent()).authorityCode(authority)
+        .status(PostStatus.CREATED).build();
   }
 
 }
