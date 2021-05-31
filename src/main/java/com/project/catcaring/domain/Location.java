542524b14e4b4ffa32e24fffd659ea0437905328
sunny <parks38@uw.edu>
2021-05-31 22:55:45 +0900

    fix: location builder 포함

--- src/main/java/com/project/catcaring/domain/Location.java
+++ src/main/java/com/project/catcaring/domain/Location.java
@@ -2,6 +2,7 @@ package com.project.catcaring.domain;
 
 import com.project.catcaring.domain.user.Authority;
 import com.project.catcaring.domain.user.User.Address;
+import com.project.catcaring.dto.post.PostInfoRequest;
 import lombok.Builder;
 
 @Builder
@@ -11,4 +12,14 @@ public class Location {
   private final Address location;
   private final String locationDetail;
   private final Authority authorityCode;
+
+  public static Location generate(PostInfoRequest postInfoRequest, Long postId) {
+    Authority authority = postInfoRequest.getLocationAuthorityCode();
+    if(authority == null) {
+      authority = Authority.ACTIVE_MEM;
+    }
+    return Location.builder().postId(postId).location(postInfoRequest.getLocation())
+        .locationDetail(postInfoRequest.getLocationDetail())
+        .authorityCode(authority).build();
+  }
 }
