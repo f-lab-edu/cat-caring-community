3dbd2211b32f4e139a8228b7bdc24cfff35bfcff
sunny <parks38@uw.edu>
2021-05-31 22:56:31 +0900

    fix: 역할 분리하여 리팩토링

--- src/main/java/com/project/catcaring/domain/Tag.java
+++ src/main/java/com/project/catcaring/domain/Tag.java
@@ -4,9 +4,12 @@ import lombok.Builder;
 
 @Builder
 public class Tag {
-
   private final Long id;
   private final Long postId;
   private final String tagName;
+
+  public static Tag generate(String tagName, Long postId) {
+    return Tag.builder().postId(postId).tagName(tagName).build();
+  }
 
 }
