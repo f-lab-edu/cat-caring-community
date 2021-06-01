6ed6434591a61888756e8d843dc714a43c8c3a11
sunny <parks38@uw.edu>
2021-06-01 21:15:50 +0900

    fix: post service deletePost에 대한 로직 수정

--- src/main/java/com/project/catcaring/service/PostServiceImpl.java
+++ src/main/java/com/project/catcaring/service/PostServiceImpl.java
@@ -6,7 +6,7 @@ import com.project.catcaring.domain.Post.PostStatus;
 import com.project.catcaring.domain.Tag;
 import com.project.catcaring.domain.user.Authority;
 import com.project.catcaring.dto.post.PostInfoRequest;
-import com.project.catcaring.handler.InvalidProcessException;
+import com.project.catcaring.handler.ProcessErrorException;
 import com.project.catcaring.mapper.PostMapper;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -21,65 +21,24 @@ public class PostServiceImpl implements PostService{
 
   @Override
   public void uploadPost(PostInfoRequest postInfoRequest, Long userId) {
-    Post newPost = buildPostInfo(postInfoRequest, userId);
-    postMapper.insertPost(newPost);
+    postMapper.insertPost(Post.generate(postInfoRequest, userId));
+
     Long postId = postMapper.findLastPostByUserId(userId);
 
-    Location postLocation = buildLocationInfo(postInfoRequest, postId);
-    postMapper.insertLocation(postLocation);
+    postMapper.insertLocation(Location.generate(postInfoRequest, postId));
 
     if(postInfoRequest.getTagNames() != null) {
       for(String tagName : postInfoRequest.getTagNames()) {
-        Tag postTag = Tag.builder().postId(postId).tagName(tagName).build();
-        postMapper.insertTag(postTag);
+        postMapper.insertTag(Tag.generate(tagName, postId));
       }
     }
-
-  }
-
-  private Post buildPostInfo(PostInfoRequest postInfoRequest, Long userId){
-    Authority authority = postInfoRequest.getPostAuthorityCode();
-    if(authority == null) {
-      authority = Authority.ALL;
-    }
-
-    return Post.builder().userId(userId).title(postInfoRequest.getTitle())
-                    .content(postInfoRequest.getContent()).authorityCode(authority)
-                    .status(PostStatus.CREATED).build();
-  }
-
-  private Location buildLocationInfo(PostInfoRequest postInfoRequest, Long postId) {
-    Authority authority = postInfoRequest.getLocationAuthorityCode();
-
-    if(authority == null) {
-      authority = Authority.ACTIVE_MEM;
-    }
-
-    return Location.builder().postId(postId).location(postInfoRequest.getLocation())
-            .locationDetail(postInfoRequest.getLocationDetail())
-            .authorityCode(authority).build();
-  }
-
-  @Override
-  public List<Post> viewPost(Long postId) {
-    return null;
-  }
-
-  @Override
-  public void updatePost(PostInfoRequest postInfoRequest) {
-
   }
 
   @Override
   public void deletePost(Long userId, Long postId) {
-
     boolean result = postMapper.deletePost(userId, postId);
-
     if(!result) {
-      throw new InvalidProcessException("해당 포스트가 존재하지 않습니다.");
+      throw new ProcessErrorException();
     }
-
   }
-
-
 }
