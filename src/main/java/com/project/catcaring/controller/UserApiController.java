ab431d5076e73b982d8cecb7ab6a3000547ea558
sunny <parks38@uw.edu>
2021-06-22 16:18:39 +0900

    feat: login aop 수정

--- src/main/java/com/project/catcaring/controller/UserApiController.java
+++ src/main/java/com/project/catcaring/controller/UserApiController.java
@@ -1,15 +1,15 @@
 package com.project.catcaring.controller;
 
-import static com.project.catcaring.error.HttpResponses.*;
 import static com.project.catcaring.error.HttpResponses.RESPONSE_OK;
 
-import com.project.catcaring.aop.annotation.CheckLoginAndResult;
+import com.project.catcaring.aop.annotation.CheckLogin;
 import com.project.catcaring.domain.User;
 import com.project.catcaring.dto.user.request.UserChangeRequest;
 import com.project.catcaring.service.user.LoginSessionService;
 import com.project.catcaring.service.user.UserApiServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,21 +28,21 @@ public class UserApiController {
   private final LoginSessionService loginSessionService;
 
   @GetMapping
-  @CheckLoginAndResult
+  @CheckLogin
   public ResponseEntity<User> getUserInfo() {
     return ResponseEntity.ok(userServiceImpl.getUserInfo(loginSessionService.getCurrentUserId()));
   }
 
   @DeleteMapping
-  @CheckLoginAndResult
+  @CheckLogin
   public ResponseEntity<String> deleteUser() {
       userServiceImpl.deleteUser(loginSessionService.getCurrentUserId());
       loginSessionService.logoutUser();
-      return RESPONSE_OK;
+      return new ResponseEntity<>("user delete completed", HttpStatus.NO_CONTENT);
   }
 
   @PatchMapping
-  @CheckLoginAndResult
+  @CheckLogin
   public ResponseEntity<String> updateUser(@RequestBody UserChangeRequest userChangeRequest) {
       userServiceImpl.updateUser(userChangeRequest, loginSessionService.getCurrentUserId());
     return RESPONSE_OK;
